542dceb6619633033ed5951fd2ef62e019b5b5a0
Brent Samodien <BSamodien@users.noreply.github.com>
2018-04-30 14:56:47 -0700

    Update Solution to Latest Components

--- src/MobileApps/MyDriving/MyDriving.AzureClient/DeviceProvisionHandler.cs
+++ src/MobileApps/MyDriving/MyDriving.AzureClient/DeviceProvisionHandler.cs
@@ -1,21 +1,27 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-using Microsoft.WindowsAzure.MobileServices;
-using MyDriving.Utils;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Plugin.DeviceInfo;
 using System.Text;
+
+using Microsoft.WindowsAzure.MobileServices;
+using Plugin.DeviceInfo;
+using Newtonsoft.Json.Linq;
+
+using MyDriving.Utils;
+using MyDriving.Utils.Helpers;
+
+
+
 
 namespace MyDriving.AzureClient
 {
     public class DeviceProvisionHandler
     {
-        private const string DefaultHostName = "mydriving-vpwupcazgfita.azure-devices.net";
+        private const string DefaultHostName = "mydriving-or76fh5yqpqg2.azure-devices.net";
         private static DeviceProvisionHandler _handler;
 
         private DeviceProvisionHandler()
