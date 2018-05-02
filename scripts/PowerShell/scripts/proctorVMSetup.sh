257fcc5b721bada413b6ed7dcee2751b06157219
Oguz Pastirmaci <oguzp@microsoft.com>
2018-05-02 15:36:12 -0700

    Update proctorVMSetup.sh

--- scripts/PowerShell/scripts/proctorVMSetup.sh
+++ scripts/PowerShell/scripts/proctorVMSetup.sh
@@ -21,7 +21,13 @@ yum install -y jq
 
 echo "############### Installing Docker ###############"
 yum check-update
-curl -fsSL https://get.docker.com/ | sh
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
 systemctl start docker
-systemctl enable docker
-usermod -aG docker $(whoami)
+groupadd docker
+usermod -aG docker $USER
