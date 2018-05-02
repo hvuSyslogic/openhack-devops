971904a5f5f92c79786ea6ed5e12fc3ad58078d9
Oguz Pastirmaci <oguzp@microsoft.com>
2018-05-02 16:12:51 -0700

    Update proctorVMSetup.sh

--- scripts/PowerShell/scripts/proctorVMSetup.sh
+++ scripts/PowerShell/scripts/proctorVMSetup.sh
@@ -11,7 +11,7 @@ sudo mv linux-amd64/helm /usr/local/bin/helm
 echo "############### Installing Dotnet SDK v2.1.4 ###############"
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[packages-microsoft-com-prod]\nname=packages-microsoft-com-prod \nbaseurl= https://packages.microsoft.com/yumrepos/microsoft-rhel7.3-prod\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/dotnetdev.repo'
-sudo yum update
+sudo yum update -y
 sudo yum install -y libunwind libicu
 sudo yum install -y dotnet-sdk-2.1.4
 
