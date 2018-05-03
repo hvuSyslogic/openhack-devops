e400c218bca33ff9fb3d11184de999ca493de098
Oguz Pastirmaci <oguzp@microsoft.com>
2018-05-03 11:08:52 -0700

    Update proctorVMSetup.sh

--- scripts/PowerShell/scripts/proctorVMSetup.sh
+++ scripts/PowerShell/scripts/proctorVMSetup.sh
@@ -1,3 +1,6 @@
+echo "############### Removing requiretty ###############""
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
 echo "############### Installing Azure CLI v2.0.31 ###############"
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
