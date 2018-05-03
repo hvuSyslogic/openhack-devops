eb12f0211464d0e7893b487c72d0513ea7173852
Richard Guthrie <rguthriemsft@users.noreply.github.com>
2018-05-03 10:21:09 -0700

    Completed test for trip service (#42)

--- src/MobileAppServiceV2/TripService/test/api_test.go
+++ src/MobileAppServiceV2/TripService/test/api_test.go
@@ -2,10 +2,11 @@ package tripsgo
 
 import (
 	"bytes"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/Azure-Samples/openhack-devops/src/MobileAppServiceV2/TripService/tripsgo"
 
 	tripSvc "github.com/Azure-Samples/openhack-devops/src/MobileAppServiceV2/TripService/tripsgo"
 	"github.com/gorilla/mux"
@@ -36,13 +37,13 @@ func testAPI(router *mux.Router, method, URL, body string) *httptest.ResponseRec
 }
 
 func runAPITests(t *testing.T, router *mux.Router, tests []apiTestCase) {
-	for _, test := range tests {
-		res := testAPI(router, test.method, test.url, test.body)
-		test.actualResponse = res.Body.String()
-		log.Printf(test.actualResponse)
-		assert.Equal(t, test.status, res.Code, test.tag)
-		if test.expectedResponse != "" {
-			assert.JSONEq(t, test.expectedResponse, res.Body.String(), test.tag)
+	for i := 0; i < len(tests); i += 1 {
+		res := testAPI(router, tests[i].method, tests[i].url, tests[i].body)
+		tests[i].actualResponse = res.Body.String()
+		tripsgo.LogToConsole(tests[i].actualResponse)
+		assert.Equal(t, tests[i].status, res.Code, tests[i].tag)
+		if tests[i].expectedResponse != "" {
+			assert.JSONEq(t, tests[i].expectedResponse, res.Body.String(), tests[i].tag)
 		}
 	}
 }
